f25211917bfce25febd72ad3b0f027e72d58601b
ogeesan <stugeor@hotmail.com>
2020-10-02 12:04:00 +1000

    Prevent overlap, improve imread() speed, add directory return option

--- Fluorescence Extraction/readroi_GS.m
+++ Fluorescence Extraction/readroi_GS.m
@@ -1,6 +1,7 @@
-function readroi_GS(varargin)
+function readroi_GS(opts)
 %{
-version: 200930
+George Stuyt
+version: 201002
 I got this from LG. It was credited to "INC 2017 by Marina and Pedro."
 
 Takes ROI set (.zip) and calculates average fluorescence for each ROI on
@@ -18,32 +19,57 @@ Changes:
 %% Select data to be read
 % get .zip containing ImageJ ROIs
 current_directory = pwd;
-switch numel(varargin) > 0
-    case false
-        
-        [fname, basedir] = uigetfile('*.*','Select RoiSet.zip file');
-        cd(basedir);
-        mcdir = uigetdir([], 'Select folder containing .tif files to read.'); % get folder where files to read are
-    otherwise
-        basedir = varargin{1};
-        fname = varargin{2};
-        mcdir = varargin{3};
+
+if nargin == 0
+    opts.default = true;
 end
+
+if ~isfield(opts,'preventoverlap'); opts.preventoverlap = true;end % set overlapping pixels to false
+if ~isfield(opts,'returntocd'); opts.returntocd = false;end % after completion return to original folder
+
+if opts.returntocd
+    current_directory = pwd; % the directory before the function moves things around
+end
+
+% -- Define core names
+% basedir = dir of RoiSet.zip that Facrosstrials will be saved to
+% fname = filename of the RoiSet.zip that is to be used
+% mcdir = dir of the files to have their fluorescence extracted
+if any(isfield(opts,{'basedir','fname','mcdir'}))
+    % if it's specified (e.g. you're using a loop)
+    basedir = opts.basedir;
+    fname = opts.fname;
+    mcdir = opts.mcdir;
+else % otherwise just get user input to define file locations
+    [fname, basedir] = uigetfile('*.*','Select RoiSet.zip file');
+    cd(basedir);
+    mcdir = uigetdir([], 'Select folder containing .tif files to read.'); % get folder where files to read are
+end
+
+% switch numel(varargin) > 0
+%     case false
+%         
+%         [fname, basedir] = uigetfile('*.*','Select RoiSet.zip file');
+%         cd(basedir);
+%         mcdir = uigetdir([], 'Select folder containing .tif files to read.'); % get folder where files to read are
+%     otherwise
+%         basedir = varargin{1};
+%         fname = varargin{2};
+%         mcdir = varargin{3};
+% end
 
 
 [rois] = ReadImageJROI(fullfile(basedir,fname)); % read in ROIs, cells with structure containing ROI details
 nROIs = size(rois,2);
 
-
-
+% -- Get files to read
 cd(mcdir)
-
 filelist = dir('*.tif*'); % get a list of all .tif files in the folder
 fnames = {filelist.name}'; % get filenames
 nFiles = size(fnames, 1);
 
 % define name of output file
-savepath = fullfile(basedir, 'Facrosstrials.mat'); % set save location of .mat to same location as RoiSet.zip
+savepath = fullfile(basedir, 'Facrosstrials.mat');
 
 %% Read fluorescence
 % timing stuff
@@ -67,19 +93,26 @@ for xfile = 1:nFiles
         [X,Y] = meshgrid(1:tiftag(1).Width,1:tiftag(1).Height); % create grids with arbitrary numbers
         roimasks = false(tiftag(1).Width,tiftag(1).Height,nROIs); % mask is a width x height x nOIs logical that will define pixels of each ROI
         
+        % -- Convert coordinates ROI outlines into x-y logical mask
         for xroi = 1:nROIs
             roicoords = rois{xroi}.mnCoordinates + 0.5; % retrieve roi coordinates and add 0.5 to align with what was drawn in ImageJ
             [in, on] = inpolygon(X,Y,roicoords(:,1),roicoords(:,2)); % check which pixels are (entirely) in or on (the edge of) area defined by roicoords
             roimasks(:,:,xroi) = in & ~on; % define roi as pixels within but not on the edge of the polygon
 %             roimasks(:,:,xroi) = inpolygon(X,Y,rois{xroi}.mnCoordinates(:,1),rois{xroi}.mnCoordinates(:,2)); % the old code
         end
+        
+        % -- Remove overlapping true values
+        if opts.preventoverlap % if the setting hasn't been changed
+            
+        end
+        
     end
 
     roimeans{xfile} = NaN(nROIs,nFrames); % initialise matrix
     
     % -- Extract fluorescence
     for xframe = 1:nFrames
-        singleframe = imread(fullfile(mcdir,filetoRead), xframe); % load frame
+        singleframe = imread(fullfile(mcdir,filetoRead), xframe,'Info',tiftag); % load frame
         singleframe(singleframe < 100) = 0; % zero the dark noise
         
         % loop through ROIs
@@ -122,6 +155,10 @@ xlabel('Loop');ylabel('Time (s)');title('Time per loop');yline(mean(tmr.times(:,
 
 varargout{1} = roimeans;
 save(savepath, 'roimeans') % save the file
-cd(basedir) % set directory to where the file was saved
-% cd(current_directory) % return matlab to where it was
+
+if opts.returntocd
+    cd(current_directory) % return matlab to where it was
+else
+    cd(basedir) % set directory to where the file was saved
+end
 end
